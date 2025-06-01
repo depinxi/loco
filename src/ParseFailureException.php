98be52480c39d26323ee01de5a7583c81104fe4d
Renaud Guillard <dev@nore.fr>
2025-06-01 19:44:52 +0200

    Fix PHP 8.4+ compatibility
    Fix implicit nullable parameter deprecation warnings

--- src/ParseFailureException.php
+++ src/ParseFailureException.php
@@ -7,7 +7,7 @@ use Exception;
 // parse. Usually non-fatal and almost always caught
 class ParseFailureException extends Exception
 {
-    public function __construct($message, $i, $string, $code = 0, Exception $previous = null)
+    public function __construct($message, $i, $string, $code = 0, ?Exception $previous = null)
     {
         $message .= " at position " . var_export($i, true) . " in string " . var_export($string, true);
         parent::__construct($message, $code);
